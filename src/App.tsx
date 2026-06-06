31cc679b85dbab6c9fb2b8c98a239cee3c27aea4
rhokeeb <rhokeebsanni@gmail.com>
2026-06-07 00:24:32 +0100

    create portfolio sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
-const App = () => {
+function App() {
   return (
-    <div>App</div>
-  )
+    <main className="bg-black text-white">
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </main>
+  );
 }
 
-export default App
+export default App;
